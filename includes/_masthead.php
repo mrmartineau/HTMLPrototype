fdde1eb93a70649e9a05de339d5923e73dfa50ca
Zander Martineau <zander@martineau.tv>
2012-02-23 10:09:56 +0000

    make nav 'generic'

--- includes/_masthead.php
+++ includes/_masthead.php
@@ -1,9 +1,9 @@
 <header id="header" class="row" role="banner">
 	<nav id="nav" class="grid_10 column text_centre" role="navigation">
-		<a href="services.php" title="Services">Services</a>
-		<a href="plastik.php" title="Plastik">Plastik</a>
-		<a href="/" title="Beyond Productions"><img src="images/front/ui/beyond-logo.png" alt="Beyond Productions logo"></a>
-		<a href="about.php" title="About">About</a>
-		<a href="contact.php" title="Contact">Contact</a>
+		<a href="#nowhere" title="Lorum ipsum dolor sit amet">Lorem</a>
+		<a href="#nowhere" title="Aliquam tincidunt mauris eu risus">Aliquam</a>
+		<a href="#nowhere" title="Morbi in sem quis dui placerat ornare">Morbi</a>
+		<a href="#nowhere" title="Praesent dapibus, neque id cursus faucibus">Praesent</a>
+		<a href="#nowhere" title="Pellentesque fermentum dolor">Pellentesque</a>
 	</nav>
 </header>
